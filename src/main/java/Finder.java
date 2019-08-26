ebd8160fdcbd378a39f0d22b171c701189b4d8d5
KuoAiTe <robinsa87@gmail.com>
2019-08-26 12:01:15 -0500

    update

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,20 +1,20 @@
-public class Finder {
-    public Integer findMin(int[] intArray) {
+class Finder {
+    Integer findMin(int[] intArray) {
         int arraySize;
         if (intArray == null || (arraySize = intArray.length) == 0)
             return null;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < arraySize; i++)
-            min = (intArray[i] < min) ? intArray[i] : min;
+            min = Math.min(intArray[i],min);
         return min;
     }
-    public Integer findMax(int[] intArray) {
+    Integer findMax(int[] intArray) {
         int arraySize;
         if (intArray == null || (arraySize = intArray.length) == 0)
             return null;
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < arraySize; i++)
-            max = (intArray[i] > max) ? intArray[i] : max;
+            max = Math.max(intArray[i],max);
         return max;
     }
 }
